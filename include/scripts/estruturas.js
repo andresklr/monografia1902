c5585ae6c0486b7bf259026a4ebf06cc7cb6fcca
Andres Giraldo <andres1@outlook.com>
2019-11-17 15:24:25 -0300

    Remoção da raíz funcionando

--- include/scripts/estruturas.js
+++ include/scripts/estruturas.js
@@ -29,7 +29,7 @@ function e_BuscaArvore(valorBusca, operacao) {
     else {
         result = e_BuscaNo(arvore.raiz, valorBusca, operacao, doDelete);
         if (doDelete.value === true && operacao === 'R') {
-            e_RemoveNo(arvore.raiz, 'A');
+            e_RemoveArvore();
             doDelete.value = false;
         }
         return result;
@@ -77,6 +77,51 @@ function e_BuscaNo(No, valorBusca, operacao, doDelete) {
     }
 }
 
+function e_RemoveArvore() {
+    svg_removeSvgs(arvore.raiz, false);
+    if (arvore.raiz.no_esquerda === null) {
+        if (arvore.raiz.no_direita === null) {
+            main_initiate();
+            criar_arvore();
+        }
+        else {
+            svg_Substituir_No(arvore.raiz.no_direita, arvore.raiz, true);
+            arvore.raiz = arvore.raiz.no_direita;
+        }
+    }
+    else {
+        if (arvore.raiz.no_direita === null) {
+            svg_Substituir_No(arvore.raiz.no_esquerda, arvore.raiz, true);
+            arvore.raiz = arvore.raiz.no_esquerda;
+        }
+        else {
+            noIt = arvore.raiz.no_esquerda;
+            if (noIt.no_direita === null) {
+                svg_Substituir_No(noIt, arvore.raiz, true);
+                noIt.no_direita = arvore.raiz.no_direita;
+                No = noIt;
+            }
+            else {
+                while (noIt.no_direita.no_direita !== null) {
+                    noIt = noIt.no_direita;
+                }
+                novoNo = noIt.no_direita;
+                svg_Substituir_No(novoNo, arvore.raiz, false);
+                svg_Substituir_No(novoNo.no_esquerda, novoNo, false);
+                noIt.no_direita = novoNo.no_esquerda;
+                svg_removeSvgs(noIt, false);
+                novoNo.no_esquerda = arvore.raiz.no_esquerda;
+                novoNo.no_direita = arvore.raiz.no_direita;
+                arvore.raiz = novoNo;
+                e_graficosNo(noIt, false);
+            }
+
+        }
+    }
+    svg_removeSvgs(arvore.raiz, false);
+    e_graficosNo(arvore.raiz, false);
+}
+
 function e_RemoveNo(No, lado) {
     //Observação, precisou ser feito desta forma, duplicando a função para o lado esquerdo e direito porque o javascript não aceita passada de parâmetro por referência, dessa forma, precisou ser enviado
     //o nó pai, e teve que fazer a lógica para caso o nó a remover fosse do lado esquerdo e caso fosse para o lado direito
@@ -114,98 +159,56 @@ function e_RemoveNo(No, lado) {
                     svg_Substituir_No(novoNo, No.no_esquerda, false);
                     svg_Substituir_No(novoNo.no_esquerda, novoNo, false);
                     noIt.no_direita = novoNo.no_esquerda;
-                    svg_removeSvgs(noIt, false);                                        
+                    svg_removeSvgs(noIt, false);
                     novoNo.no_esquerda = No.no_esquerda.no_esquerda;
                     novoNo.no_direita = No.no_esquerda.no_direita;
-                    No.no_esquerda = novoNo;                    
-                    e_graficosNo(noIt, false);                    
+                    No.no_esquerda = novoNo;
+                    e_graficosNo(noIt, false);
                 }
             }
         }
     }
-    else if (lado === 'D') {
+    else //if (lado === 'D') {
         svg_removeSvgs(No.no_direita, false);
-        if (No.no_direita.no_esquerda === null) {
-            if (No.no_direita.no_direita === null) {
-                svg_removeSvgs(No.no_direita, false);
-                No.no_direita = null;
-            }
-            else {
-                svg_Substituir_No(No.no_direita.no_direita, No.no_direita, true);
-                No.no_direita = No.no_direita.no_direita;
-            }
+    if (No.no_direita.no_esquerda === null) {
+        if (No.no_direita.no_direita === null) {
+            svg_removeSvgs(No.no_direita, false);
+            No.no_direita = null;
         }
         else {
-            if (No.no_direita.no_direita === null) {
-                svg_Substituir_No(No.no_direita.no_esquerda, No.no_direita, true);
-                No.no_direita = No.no_direita.no_esquerda;
-            }
-            else {
-                noIt = No.no_direita.no_esquerda;
-                if (noIt.no_direita === null) {
-                    svg_Substituir_No(noIt, No.no_direita, true);
-                    noIt.no_direita = No.no_direita.no_direita;
-                    No.no_direita = noIt;                    
-                }
-                else {
-                    while (noIt.no_direita.no_direita !== null) {
-                        noIt = noIt.no_direita;
-                    }
-                    novoNo = noIt.no_direita;
-                    svg_Substituir_No(novoNo, No.no_direita, false);
-                    svg_Substituir_No(novoNo.no_esquerda, novoNo, false);
-                    noIt.no_direita = novoNo.no_esquerda;
-                    svg_removeSvgs(noIt, false);                    
-                    novoNo.no_esquerda = No.no_direita.no_esquerda;
-                    novoNo.no_direita = No.no_direita.no_direita;
-                    No.no_direita = novoNo;                    
-                    e_graficosNo(noIt, false);
-                }
-            }
+            svg_Substituir_No(No.no_direita.no_direita, No.no_direita, true);
+            No.no_direita = No.no_direita.no_direita;
         }
     }
-    else { //'A' Raiz da árvore
-        if (No.no_esquerda === null) {
-            if (No.no_direita === null) {
-                main_initiate();
-                criar_arvore();
-            }
-            else {
-                svg_Substituir_No(No.no_direita, No, true);
-                No = No.no_direita;
-            }
+    else {
+        if (No.no_direita.no_direita === null) {
+            svg_Substituir_No(No.no_direita.no_esquerda, No.no_direita, true);
+            No.no_direita = No.no_direita.no_esquerda;
         }
         else {
-            if (No.no_direita === null) {
-                svg_Substituir_No(No.no_esquerda, No, true);
-                No = No.no_esquerda;
+            noIt = No.no_direita.no_esquerda;
+            if (noIt.no_direita === null) {
+                svg_Substituir_No(noIt, No.no_direita, true);
+                noIt.no_direita = No.no_direita.no_direita;
+                No.no_direita = noIt;
             }
             else {
-                noIt = No.no_esquerda;
-                if (noIt.no_direita === null) {
-                    svg_Substituir_No(noIt, No, true);
-                    noIt.no_direita = No.no_direita;
-                    No = noIt;                    
+                while (noIt.no_direita.no_direita !== null) {
+                    noIt = noIt.no_direita;
                 }
-                else {
-                    while (noIt.no_direita.no_direita !== null) {
-                        noIt = noIt.no_direita;
-                    }
-                    novoNo = noIt.no_direita;
-                    svg_Substituir_No(novoNo, No, false);
-                    svg_Substituir_No(novoNo.no_esquerda, novoNo, false);
-                    noIt.no_direita = novoNo.no_esquerda;
-                    svg_removeSvgs(noIt, false);                    
-                    novoNo.no_esquerda = No.no_esquerda;
-                    novoNo.no_direita = No.no_direita;
-                    No = novoNo;                    
-                    e_graficosNo(noIt, false);
-                }
+                novoNo = noIt.no_direita;
+                svg_Substituir_No(novoNo, No.no_direita, false);
+                svg_Substituir_No(novoNo.no_esquerda, novoNo, false);
+                noIt.no_direita = novoNo.no_esquerda;
+                svg_removeSvgs(noIt, false);
+                novoNo.no_esquerda = No.no_direita.no_esquerda;
+                novoNo.no_direita = No.no_direita.no_direita;
+                No.no_direita = novoNo;
+                e_graficosNo(noIt, false);
             }
         }
+        //}
     }
-    svg_removeSvgs(No, false);
-    e_graficosNo(No, false);
 }
 
 function e_Altura_Arvore(No, h) {
